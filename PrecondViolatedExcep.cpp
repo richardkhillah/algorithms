b5d0293719784dd4a589df710784d6b467bd755e
WhiteRabbit27 <RKhillah@mail.ccsf.edu>
2017-01-23 21:59:33 -0800

    first commit

---
+++ PrecondViolatedExcep.cpp
@@ -0,0 +1,8 @@
+/** @file PrecondViolatedExcep.cpp */
+#include "PrecondViolatedExcep.h"
+
+PrecondViolatedExcep::PrecondViolatedExcep(const string& message): logic_error("Precondition Violated Exception: " + message)
+{
+}  // end constructor
+
+// End of implementation file.
